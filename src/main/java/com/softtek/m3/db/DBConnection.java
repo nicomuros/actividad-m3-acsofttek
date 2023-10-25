06586f2b979731a2d83fe5b35be0ee3e9953b52e
Nicolás Muros <nicomuros@gmail.com>
2023-10-25 15:42:23 -0300

    limpieza interfaz DBConnection

--- src/main/java/com/softtek/m3/db/DBConnection.java
+++ src/main/java/com/softtek/m3/db/DBConnection.java
@@ -3,6 +3,6 @@ package com.softtek.m3.db;
 import java.sql.Connection;
 
 public interface DBConnection {
-    public Connection establecerConexion();
-    public void createTable();
+    Connection establecerConexion();
+    void createTable();
 }
