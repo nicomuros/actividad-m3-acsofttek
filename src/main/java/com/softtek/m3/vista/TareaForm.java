7a47e81494e1eb6b87cf5638f5feebf7145b8d36
Nicolás Muros <nicomuros@gmail.com>
2023-10-26 15:40:09 -0300

    Formulario listo

--- src/main/java/com/softtek/m3/vista/TareaForm.java
+++ src/main/java/com/softtek/m3/vista/TareaForm.java
@@ -6,13 +6,15 @@ import com.softtek.m3.servicio.TareaServicio;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.util.List;
 import java.awt.*;
 
 public class TareaForm extends JFrame {
+
+    // Se declara la dependencia que será inyectada
     TareaServicio tareaServicio;
+
     private JPanel panelPrincipal;
     private JTable tablaTareas;
     private JScrollPane scrollPane;
@@ -22,40 +24,85 @@ public class TareaForm extends JFrame {
     private JButton modificarButton;
     private JButton eliminarButton;
     private JLabel resultadoLabel;
+    private JLabel idSelected;
     private DefaultTableModel tablaModeloTareas;
 
     public TareaForm(TareaServicio tareaServicio){
+
+        // Se instancia la dependendia del servicio.
         this.tareaServicio = tareaServicio;
+
+        // Se inicia el formulario
         iniciarForm();
-        agregarButton.addActionListener(e -> handleAgregarTarea());
-        modificarButton.addActionListener(e -> {
 
-        });
-        eliminarButton.addActionListener(e -> {
-
+        // Configuración de botones
+        agregarButton.addActionListener(e -> manejarAgregarTarea());
+        modificarButton.addActionListener(e -> manejarModificarTarea());
+        eliminarButton.addActionListener(e -> manejarEliminarTarea());
+        tablaTareas.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                manejarSeleccionTarea();
+            }
         });
     }
 
+    private void createUIComponents() {
+        iniciarTabla();
+        listarTareas();
+    }
+
     private void iniciarForm(){
+
+        // Configuración del panel
         setContentPane(panelPrincipal);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(900, 900);
 
-        // Para obtener las dimensiones de la ventana y ubicarlo en el centro
+        // Ubicar el form en el centro de la pantalla
         Toolkit toolkit = Toolkit.getDefaultToolkit();
-
         Dimension screenSize = toolkit.getScreenSize();
         int x = (getWidth()/2);
         int y = (screenSize.height - getHeight())/2;
         setLocation(x, y);
     }
 
-    private void handleAgregarTarea(){
+    private void iniciarTabla(){
+
+        // Configuración de los títulos de la tabla
+        this.tablaModeloTareas = new DefaultTableModel(0, 3) {
+            // Configuración para que no se puedan modificar las celdas desde la interfaz
+            @Override
+            public boolean isCellEditable(int row, int column){ return false; }
+        };
+
+        // Titulo de las columnas
+        String[] titulosColumnas = {"ID", "Titulo", "Descripcion"};
+        this.tablaModeloTareas.setColumnIdentifiers(titulosColumnas);
+
+        // Instancia objeto de JTable
+        this.tablaTareas = new JTable(tablaModeloTareas);
+
+        // Configurar ancho columnas
+        this.tablaTareas.getColumnModel().getColumn(0).setMaxWidth(20);
+        this.tablaTareas.getColumnModel().getColumn(1).setMaxWidth(230);
+
+        // Configurar imposibilidad de seleccionar más de un registro
+        this.tablaTareas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+    }
+
+    private void manejarAgregarTarea(){
+
         // Leer los valores del formulario
         String titulo = tituloUserInput.getText();
         String descripcion = descripcionUserInput.getText();
+
         try {
+
+            // Validaciones
             if (titulo.isBlank() && descripcion.isBlank()) {
                 throw new DatosInvalidosException("Error: Debe introducir un título y una descripción para agregar la tarea. No se admiten espacios en blanco.");
             }
@@ -65,14 +112,115 @@ public class TareaForm extends JFrame {
             if (descripcion.isBlank()){
                 throw new DatosInvalidosException("Error: Debe introducir un título. No se admiten espacios en blanco.");
             }
-            tareaServicio.agregarTarea(titulo, descripcion);
+
+            // Se solicita al repositorio agregar la nueva tarea
+            Tarea tarea = new Tarea(titulo, descripcion);
+            tareaServicio.agregarTarea(tarea);
+            manejarExito("Tarea agregada correctamente.");
+
+        } catch (Exception e){
+
+            // Se muestra mensaje de error
+            manejarExcepcion(e);
+        } finally {
+
+            // Actualización de tabla, y limpieza de campos
             listarTareas();
-            manejarExito("Tarea agregada correctamente");
+            limpiarCampos();
+        }
+    }
+
+    private void manejarSeleccionTarea(){
+
+        // Se obtiene el número de fila que seleccionó
+        int filaSeleccionada = tablaTareas.getSelectedRow();
+
+        // Se comprueba que se haya seleccionado una fila.
+        if (filaSeleccionada != -1) {
+
+            // Recuperación de datos de la tabla
+            String tareaIdString = tablaTareas.getModel()
+                    .getValueAt(filaSeleccionada, 0)
+                    .toString();
+
+            String titulo = tablaTareas.getModel().getValueAt(filaSeleccionada, 1).toString();
+            String descripcion = tablaTareas.getModel().getValueAt(filaSeleccionada, 2).toString();
+
+            // Se muestran los datos en los fieldtext
+            idSelected.setText(tareaIdString);
+            tituloUserInput.setText(titulo);
+            descripcionUserInput.setText(descripcion);
+        }
+    }
+
+    private void manejarModificarTarea(){
+
+        // Se recuperan los datos de los fieldset
+        String tareaIdString = idSelected.getText();
+        String titulo = tituloUserInput.getText();
+        String descripcion = descripcionUserInput.getText();
+
+        try {
+
+            // Validaciones
+            if (tareaIdString.isBlank()) throw new DatosInvalidosException("Error: No seleccionó ninguna tarea para modificar.");
+            if (titulo.isBlank() && descripcion.isBlank()) {
+                throw new DatosInvalidosException("Error: Debe introducir un título y una descripción para modificar la tarea. No se admiten espacios en blanco.");
+            }
+            if (titulo.isBlank()){
+                throw new DatosInvalidosException("Error: Debe introducir un título. No se admiten espacios en blanco.");
+            }
+            if (descripcion.isBlank()){
+                throw new DatosInvalidosException("Error: Debe introducir un título. No se admiten espacios en blanco.");
+            }
+
+            // Se obtiene el ID de la tarea
+            Integer tareaId = Integer.parseInt(tareaIdString);
+
+            // Se crea un objeto Tarea con los datos modificados
+            Tarea tareaModificada = new Tarea();
+            tareaModificada.setId(tareaId);
+            tareaModificada.setTitulo(titulo);
+            tareaModificada.setDescripcion(descripcion);
+
+            // Solicitud al servicio de modificar la tarea
+            tareaServicio.modificarTarea(tareaModificada);
+            manejarExito("Tarea modificada exitosamente.");
+
+        } catch (Exception e){
+
+            // Se muestra mensaje de error
+            manejarExcepcion(e);
+        } finally {
+
+            // Actualización de tabla, y limpieza de campos
+            listarTareas();
+            limpiarCampos();
+        }
+    }
+
+    private void manejarEliminarTarea(){
+        String tareaIdString = idSelected.getText();
+        try {
+            if (tareaIdString.isBlank()) throw new DatosInvalidosException("Error: No seleccionó ninguna tarea para modificar.");
+
+            Integer tareaId = Integer.parseInt(tareaIdString);
+            tareaServicio.eliminarTarea(tareaId);
+
+            manejarExito("Tarea modificada exitosamente.");
+
         } catch (Exception e){
             manejarExcepcion(e);
+        } finally {
+            listarTareas();
+            limpiarCampos();
         }
+    }
 
-
+    private void limpiarCampos(){
+        idSelected.setText("");
+        tituloUserInput.setText("");
+        descripcionUserInput.setText("");
     }
 
     private void manejarExcepcion(Exception e){
@@ -84,35 +232,15 @@ public class TareaForm extends JFrame {
         resultadoLabel.setForeground(Color.black);
         resultadoLabel.setText(mensaje);
     }
-    private void createUIComponents() {
-        iniciarTabla();
-        listarTareas();
-    }
-
-    private void iniciarTabla(){
-        // Configuramos los títulos de la tabla
-        this.tablaModeloTareas = new DefaultTableModel(0, 3);
-        String[] titulosColumnas = {"ID", "Titulo", "Descripcion"};
-        this.tablaModeloTareas.setColumnIdentifiers(titulosColumnas);
-
-        // Configurar ancho columnas
-
-
-        // Instanciamos el objeto de JTable
-        this.tablaTareas = new JTable(tablaModeloTareas);
-        this.tablaTareas.getColumnModel().getColumn(0).setMaxWidth(20);
-        this.tablaTareas.getColumnModel().getColumn(1).setMaxWidth(230);
-
-
-    }
 
     private void listarTareas(){
         // Limpiar la tabla
         tablaModeloTareas.setRowCount(0);
 
-        // Obtener los libros de la base de datos
+        // Se almacenan las tareas provistas por el servicio
         List<Tarea> listaDeTareas = tareaServicio.obtenerTareas();
 
+        // Se rellena la tabla
         listaDeTareas.forEach((tarea) -> {
             Object[] renglonTarea = {
                     tarea.getId(),
