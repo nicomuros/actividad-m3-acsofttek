59d58f655cf2e4d331944492b5361d5d5d084796
Nicolás Muros <nicomuros@gmail.com>
2023-10-26 12:14:39 -0300

    vista -> mostrar tareas, agregar tarea

--- src/main/java/com/softtek/m3/vista/TareaForm.java
+++ src/main/java/com/softtek/m3/vista/TareaForm.java
@@ -1,10 +1,13 @@
 package com.softtek.m3.vista;
 
+import com.softtek.m3.excepciones.DatosInvalidosException;
 import com.softtek.m3.modelo.Tarea;
 import com.softtek.m3.servicio.TareaServicio;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 import java.awt.*;
 
@@ -24,6 +27,13 @@ public class TareaForm extends JFrame {
     public TareaForm(TareaServicio tareaServicio){
         this.tareaServicio = tareaServicio;
         iniciarForm();
+        agregarButton.addActionListener(e -> handleAgregarTarea());
+        modificarButton.addActionListener(e -> {
+
+        });
+        eliminarButton.addActionListener(e -> {
+
+        });
     }
 
     private void iniciarForm(){
@@ -41,20 +51,62 @@ public class TareaForm extends JFrame {
         setLocation(x, y);
     }
 
+    private void handleAgregarTarea(){
+        // Leer los valores del formulario
+        String titulo = tituloUserInput.getText();
+        String descripcion = descripcionUserInput.getText();
+        try {
+            if (titulo.isBlank() && descripcion.isBlank()) {
+                throw new DatosInvalidosException("Error: Debe introducir un título y una descripción para agregar la tarea. No se admiten espacios en blanco.");
+            }
+            if (titulo.isBlank()){
+                throw new DatosInvalidosException("Error: Debe introducir un título. No se admiten espacios en blanco.");
+            }
+            if (descripcion.isBlank()){
+                throw new DatosInvalidosException("Error: Debe introducir un título. No se admiten espacios en blanco.");
+            }
+            tareaServicio.agregarTarea(titulo, descripcion);
+            listarTareas();
+            manejarExito("Tarea agregada correctamente");
+        } catch (Exception e){
+            manejarExcepcion(e);
+        }
 
+
+    }
+
+    private void manejarExcepcion(Exception e){
+        resultadoLabel.setForeground(Color.red);
+        resultadoLabel.setText(e.getMessage());
+    }
+
+    private void manejarExito(String mensaje){
+        resultadoLabel.setForeground(Color.black);
+        resultadoLabel.setText(mensaje);
+    }
     private void createUIComponents() {
+        iniciarTabla();
+        listarTareas();
+    }
+
+    private void iniciarTabla(){
         // Configuramos los títulos de la tabla
         this.tablaModeloTareas = new DefaultTableModel(0, 3);
         String[] titulosColumnas = {"ID", "Titulo", "Descripcion"};
         this.tablaModeloTareas.setColumnIdentifiers(titulosColumnas);
 
+        // Configurar ancho columnas
+
+
         // Instanciamos el objeto de JTable
         this.tablaTareas = new JTable(tablaModeloTareas);
+        this.tablaTareas.getColumnModel().getColumn(0).setMaxWidth(20);
+        this.tablaTareas.getColumnModel().getColumn(1).setMaxWidth(230);
 
-        listarLibros();
+
     }
 
-    private void listarLibros(){
+    private void listarTareas(){
         // Limpiar la tabla
         tablaModeloTareas.setRowCount(0);
 
